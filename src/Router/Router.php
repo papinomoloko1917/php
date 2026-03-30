91f9b99d2ea3208ce012f9134bbda796ec9f62bf
papinomoloko1917 <varivodamax88@gmail.com>
2026-03-30 21:50:58 +0300

    update

--- src/Router/Router.php
+++ src/Router/Router.php
@@ -6,5 +6,10 @@ class Router
 {
     private array $routes = [];
 
+    public function __construct()
+    {
+        $this->routes = include_once dirname(__DIR__) . '/routes/routes.php';
+    }
+
     public function resolve($url, $pageDir) {}
 }
