e076c80fdf6fd0b0fcc5be3f6b13d1555eed058e
papinomoloko1917 <varivodamax88@gmail.com>
2026-05-25 22:12:05 +0300

    update

--- public/index.php
+++ public/index.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Support\Env;
+
 define('APP_PATH', dirname(__DIR__));
 
 require APP_PATH . '/vendor/autoload.php';
 
-dump($_ENV);
+$filePath = APP_PATH . '/.testFile';
+
+Env::write($filePath);
