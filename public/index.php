45ba35ef74614992d2f8bacbc69315230aa6755a
papinomoloko1917 <varivodamax88@gmail.com>
2026-06-07 21:57:25 +0300

    update

--- public/index.php
+++ public/index.php
@@ -2,20 +2,12 @@
 
 declare(strict_types=1);
 
-use App\Calculator;
-use App\View;
+use App\Database\Database;
 
 define('APP_PATH', dirname(__DIR__));
 
 require APP_PATH . '/vendor/autoload.php';
 
-$calculatePage = new View();
-echo $calculatePage->page();
+$database = Database::fromEnv();
 
-if ($_SERVER['REQUEST_URI'] === '/calculate' && $_SERVER['REQUEST_METHOD'] === 'POST') {
-    $operation = $_POST['operation'];
-    $num1 = floatval($_POST['num1']);
-    $num2 = floatval($_POST['num2']);
-    $calculate = new Calculator($operation, $num1, $num2);
-    echo $calculate->calculate();
-}
+$conn = $database->conn();
